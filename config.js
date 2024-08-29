a0d6bf365d7f742ce5a87242d1fcde4a390aac54
RoynBosses09 <roynajack303@gmail.com>
2024-08-29 14:33:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
+global.owner = '263774918784'
 global.nomerowner = ["2347043759577"]
 
 // Apikey 
